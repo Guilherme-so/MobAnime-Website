419ff8b4757cc45c78f9115ded07c3fcdd307da7
Guilherme013S <guilhermemagno09@.com>
2022-11-14 12:40:22 -0300

    Adding Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import style from "./footer.module.css"
+
+export const Footer = () => {
+  return (
+    <footer className={style.footer}>
+      <h2>
+        &#169; Guilherme Silva 2022
+      </h2>
+    </footer>
+  )
+}
